baf80d39e3adeaa1776d2e2da466ec28b8f99304
codyarose <crose992@gmail.com>
2022-10-21 23:07:53 -0500

    get random joke for jokes index route

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,13 +1,32 @@
+import type { Joke } from "@prisma/client";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomJoke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+
+  const data: LoaderData = { randomJoke };
+
+  return json(data);
+};
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
   return (
     <div>
-      <p>random joke:</p>
-      <p>
-        Alias unde inventore exercitationem necessitatibus eius facilis minima.
-        Molestias voluptate explicabo. Eius fuga itaque culpa deleniti tenetur
-        reprehenderit. Excepturi quos recusandae quibusdam deleniti quisquam
-        unde veritatis natus sunt.
-      </p>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>{data.randomJoke.name} Permalink</Link>
     </div>
   );
 }
